0d56d26f9f1c0c3a1317ad8b3074d6289ede5ddc
Juan <juan.mr.alencar2002@gmail.com>
2023-08-15 00:11:57 -0300

    ✨ feature: new color style guide

--- src/components/CompetenceCard/index.tsx
+++ src/components/CompetenceCard/index.tsx
@@ -6,18 +6,18 @@ interface ICompetenceCard {
 
 const CompetenceCard: FC<ICompetenceCard> = ({ props }) => {
   return (
-    <div className="flex flex-col gap-4 bg-purple500 w-full p-4 rounded-2xl">
+    <div className="flex flex-col gap-4 bg-gray-900 w-full p-4 rounded-2xl">
       <h1>ReactJS</h1>
       <div className="flex relative w-full">
-        <div className="absolute bg-purple400 rounded top-0 h-1 w-full" />
-        <div className="absolute flex bg-purple100 rounded top-0 h-1 w-1/2 items-center">
-          <div className="absolute h-3 w-3 rounded-full bg-purple100 right-0"></div>
+        <div className="absolute bg-gray-800 rounded top-0 h-1 w-full" />
+        <div className="absolute flex bg-purple-600 rounded top-0 h-1 w-1/2 items-center">
+          <div className="absolute h-3 w-3 rounded-full bg-purple-600 right-0"></div>
         </div>
       </div>
       <div className="flex w-full justify-between">
-        <p className=" text-sm text-purple100">Básico</p>
-        <p className=" text-sm text-purple100">Intermediário</p>
-        <p className=" text-sm text-purple100">Avançado</p>
+        <p className=" text-sm text-gray-400">Básico</p>
+        <p className=" text-sm text-gray-400">Intermediário</p>
+        <p className=" text-sm text-gray-400">Avançado</p>
       </div>
     </div>
   )
--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -14,7 +14,7 @@ const ProjectCard: FC<IProjectCard> = ({
   repository,
 }) => {
   return (
-    <div className="flex flex-col bg-purple500 rounded-2xl">
+    <div className="flex flex-col bg-gray-900 rounded-2xl">
       <Image
         alt="Banner do Projeto"
         width={300}
@@ -22,10 +22,10 @@ const ProjectCard: FC<IProjectCard> = ({
         src={imageUrl || DefaultImage}
         className="flex w-full max-h-40 object-cover object-center rounded-t-2xl"
       />
-      <div className="flex flex-col p-6 gap-4 border-t-4 border-purple100 max-w-[320px]">
+      <div className="flex flex-col p-6 gap-4 border-t-4 border-purple-600 max-w-[320px]">
         <div>
           <h1 className="text-xl font-bold uppercase">{title}</h1>
-          <p className="text-gray400">{description}</p>
+          <p className="text-gray-500">{description}</p>
         </div>
         <div className="flex gap-2">
           <Link href={repository}>
